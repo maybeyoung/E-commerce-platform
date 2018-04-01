c255bc91b68ab937e826231fd7218ace87e90bc1
wsj <stephen0607@qq.com>
2018-04-02 00:42:59 +0800

    用户前台模块接口

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,20 @@
+package com.mmall.common;
+
+/**
+ * @Author: wsj
+ * @Description:
+ * @Create in 下午 10:52 2018/4/1 0001
+ * @Modified by:
+ */
+public class Const {
+
+    public static final String  CURRENT_USER ="currentUser";
+
+    public interface Role{
+           int ROLE_CUSTOMER = 0;//普通用户
+           int ROLE_ADMIN = 1;//管理员
+    }
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+}
---
+++ src/main/java/com/mmall/common/ResponseCode.java
@@ -0,0 +1,31 @@
+package com.mmall.common;
+
+/**
+ * @Author: wsj
+ * @Description:
+ * @Create in 下午 9:54 2018/4/1 0001
+ * @Modified by:
+ */
+public enum ResponseCode {
+
+    SUCCESS(0,"SUCCESS"),
+    ERROR(1,"ERROR"),
+    NEED_LOGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
+
+    private final int code;
+    private final String desc;
+
+
+    ResponseCode(int code,String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode(){
+        return code;
+    }
+    public String getDesc(){
+        return desc;
+    }
+}
