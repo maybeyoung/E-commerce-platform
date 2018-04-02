38689237b77dab936f135a4611130cf8f0b729a5
wsj <stephen0607@qq.com>
2018-04-02 22:40:41 +0800

    品类模块功能的搭建，品类的增删改递归查，以及一些问题的修改

---
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -0,0 +1,44 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
+/**
+ * @Author: wsj
+ * @Description:
+ * @Create in 下午 8:41 2018/4/2 0002
+ * @Modified by:
+ */
+public interface ICategoryService {
+    /**
+     * 添加品类
+     * @param categoryName
+     * @param parentId
+     * @return
+     */
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    /**
+     *品类更新
+     * @param categoryId
+     * @param categoryName
+     * @return
+     */
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+
+    /**
+     * 获取子节点信息
+     * @param categoryId
+     * @return
+     */
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    /**
+     * 递归求出所有品类
+     * @param categoryId
+     * @return
+     */
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
+}
