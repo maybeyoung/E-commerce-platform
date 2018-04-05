a0f94dff8c9ffcb2ac9fd346b155ed237d65f5aa
wsj <stephen0607@qq.com>
2018-04-05 23:17:32 +0800

    订单跟支付宝 面对面扫码支付的对接

--- src/main/java/com/mmall/dao/OrderMapper.java
+++ src/main/java/com/mmall/dao/OrderMapper.java
@@ -1,8 +1,10 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Order;
+import org.apache.ibatis.annotations.Param;
 
 public interface OrderMapper {
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(Order record);
@@ -14,4 +16,9 @@ public interface OrderMapper {
     int updateByPrimaryKeySelective(Order record);
 
     int updateByPrimaryKey(Order record);
+
+    Order selectByUserIdAndOrderNo(@Param("userId")Integer userId, @Param("orderNo")Long orderNo);
+
+    Order selectByOrderNo(Long orderNo);
+
 }
---
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -0,0 +1,23 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.vo.OrderVo;
+
+import java.util.Map;
+
+/**
+ * @Author: wsj
+ * @Description:订单接口
+ * @Modified by:
+ */
+public interface IOrderService {
+
+    ServerResponse pay(Long orderNo, Integer userId, String path);
+
+    ServerResponse aliCallback(Map<String,String> params);
+
+    ServerResponse queryOrderPayStatus(Integer userId,Long orderNo);
+
+
+}
