c32bc21a1eea214a2abf71cd6256efd3a3a3d3a5
wsj <stephen0607@qq.com>
2018-04-11 21:06:36 +0800

    测试spring session的redis功能

--- src/main/java/com/mmall/pojo/User.java
+++ src/main/java/com/mmall/pojo/User.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable{
     private Integer id;
 
     private String username;
